8cf5d77b6eecd22b4c18be470b6c2b8d389b6f9d
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2023-02-18 18:05:32 +0000

    Moved create function to collection.db

--- BACKEND/collections.go
+++ BACKEND/collections.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Student struct {
+	Name string `bson:"name"`
+	Mark int `bson:"mark"`
+}
+
+// access database collection and returns *mongo.Client
+var gradeCollection = db().Database("students").Collection("grades")
+
+func createGrade(w http.ResponseWriter, r *http.Request) {
+
+	// add Content-type
+	w.Header().Set("Content-Type", "application/json")
+
+	var student Student
+	err := json.NewDecoder(r.Body).Decode(&student) // get data from request body 
+	if err != nil {
+		fmt.Print(err)
+	}
+	insertResult, err := gradeCollection.InsertOne(context.TODO(), student) // insert data to database
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// print/return output/result/error
+	fmt.Println("Created grade: ", insertResult)
+	json.NewEncoder(w).Encode(insertResult.InsertedID) 
+
+}
--- BACKEND/main.go
+++ BACKEND/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-
-	"go.mongodb.org/mongo-driver/mongo"
 
 	"net/http"
 
 	"github.com/gorilla/mux"
-)
-
-var (
-	GradesCollection *mongo.Collection
-	Ctx              = context.TODO()
 )
 
 func main() {
@@ -22,21 +13,5 @@ func main() {
 	s := route.PathPrefix("/api").Subrouter() //Base Path
 	//Routes
 	s.HandleFunc("/createGrade", createGrade).Methods("POST")
-	s.HandleFunc("/getAllGrades", getGrade).Methods("GET")
-	s.HandleFunc("/updateGrade", updateGrade).Methods("PUT")
-	s.HandleFunc("/deleteGrade/{id}", deleteGrade).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", s)) // Run Server
-}
-
-type Grade struct {
-	Name string `bson:"name"`
-	Mark int    `bson:"mark"`
-}
-
-func CreateGrade(g Grade) (string, error) {
-	result, err := GradesCollection.InsertOne(Ctx, g)
-	if err != nil {
-		return "0", err
-	}
-	return fmt.Sprintf("%v", result.InsertedID), err
 }
