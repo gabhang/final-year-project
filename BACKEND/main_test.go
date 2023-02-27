6f39513b168e8d6b029f2802a7a07d794a15d86c
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2023-02-27 22:57:31 +0000

    Added TestCRUDOperations to handle all CRUD test functions

--- BACKEND/main_test.go
+++ BACKEND/main_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"testing"
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func TestCRUDOperations(t *testing.T) {
+
+	// Initialize MongoDB connection
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = client.Connect(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Call test functions for CRUD operations
+	// testCreateGrade(t, client)
+	// testGetGrades(t, client)
+	// testUpdateGrade(t, client)
+	// testDeleteGrade(t, client)
+}
