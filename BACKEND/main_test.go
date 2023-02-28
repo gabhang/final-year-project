e2b41529a9b39ca0a27b7f2b1cb2c173bb9a105a
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2023-02-28 09:53:33 +0000

    Added create test case

--- BACKEND/main_test.go
+++ BACKEND/main_test.go
@@ -20,8 +20,26 @@ func TestCRUDOperations(t *testing.T) {
 	}
 
 	// Call test functions for CRUD operations
-	// testCreateGrade(t, client)
+	testCreateGrade(t, client)
 	// testGetGrades(t, client)
 	// testUpdateGrade(t, client)
 	// testDeleteGrade(t, client)
+}
+
+func testCreateGrade(t *testing.T, client *mongo.Client) {
+
+	// Access grade collection
+	gradeCollection := client.Database("students").Collection("grades")
+
+	// Create new student grade
+	student := Student{Name: "John", Mark: 99}
+	result, err := gradeCollection.InsertOne(context.Background(), student)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check if the student was created
+	if result.InsertedID == nil {
+		t.Fatal("Failed to create student grade")
+	}
 }
