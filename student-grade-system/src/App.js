77b9f94b784e80a542220e3186ef2035d8b6d1a3
LAPTOP-PPOG0DGB <ghfc101@hotmail.com>
2023-03-15 17:13:38 +0000

    Added frontend stuff

--- student-grade-system/src/App.js
+++ student-grade-system/src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
+import { Navbar, Nav, NavDropdown, Row } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Listings } from './components/listings'
 
 function App() {
   return (
-    <p>Hello</p>
+    <Router>
+        <div className="App">
+          {/* Navigation bar for the site */}
+          <Navbar sticky="top" bg="light" variant="light">
+            <Navbar.Brand href="/"><img src="atu-logo-green.png" width="100"></img></Navbar.Brand>
+            <Nav className="ml-auto">
+              <Nav.Link href="/">Listings</Nav.Link>
+              <Nav.Link href="/">Add</Nav.Link>
+            </Nav>
+          </Navbar>
+
+          {/* Tabs on the navbar are routed to different components using Routes */}
+          <Routes>
+          <Route path='/' element={< Listings />} exact />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
